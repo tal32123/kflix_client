dbeb9e9a3be001b243b11b8fac095695d996cf00
Tal Charnes <tal32123@gmail.com>
2023-03-09 21:30:14 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThumbnailComponent } from './thumbnail/thumbnail.component';
+import { GridviewComponent } from './gridview/gridview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThumbnailComponent,
+    GridviewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gridview/gridview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gridview',
+  templateUrl: './gridview.component.html',
+  styleUrls: ['./gridview.component.css']
+})
+export class GridviewComponent {
+
+}
